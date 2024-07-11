5ac61df9a5085147a1cd6280c10e02617fff20a5
aarojohn <aaromaljohn@gmail.com>
2024-07-11 15:19:27 +0530

    added next page  functionality

--- frontend/src/components/movies-list.js
+++ frontend/src/components/movies-list.js
@@ -16,16 +16,37 @@ const MoviesList = (props) => {
   const [searchRating, setSearchRating] = useState("");
   const [ratings, setRatings] = useState(["All Ratings"]);
 
+  const [currentPage, setCurrentPage] = useState(0);
+  const [entriesPerPage, setEntriesPerPage] = useState(0);
+  const [currentSearchMode, setCurrentSearchMode] = useState("");
+
+  useEffect(() => {
+    setCurrentPage(0);
+  }, [currentSearchMode]);
+
   useEffect(() => {
     retrieveMovies();
     retrieveRatings();
   }, []);
 
+  useEffect(() => {
+    retrieveNextPage();
+  }, [currentPage]);
+
+  const retrieveNextPage = () => {
+    if (currentSearchMode === "findByTitle") findByTitle();
+    else if (currentSearchMode === "findByRating") findByRating();
+    else retrieveMovies();
+  };
+
   const retrieveMovies = () => {
-    MovieDataService.getAll()
+    setCurrentSearchMode("");
+    MovieDataService.getAll(currentPage)
       .then((response) => {
         console.log(response.data);
         setMovies(response.data.movies);
+        setCurrentPage(response.data.page);
+        setEntriesPerPage(response.data.entries_per_page);
       })
       .catch((e) => {
         console.log(e);
@@ -54,7 +75,7 @@ const MoviesList = (props) => {
   };
 
   const find = (query, by) => {
-    MovieDataService.find(query, by)
+    MovieDataService.find(query, by, currentPage)
       .then((response) => {
         console.log(response.data);
         setMovies(response.data.movies);
@@ -65,10 +86,12 @@ const MoviesList = (props) => {
   };
 
   const findByTitle = () => {
+    setCurrentSearchMode("findByTitle");
     find(searchTitle, "title");
   };
 
   const findByRating = () => {
+    setCurrentSearchMode("findByRating");
     if (searchRating === "All Ratings") {
       retrieveMovies();
     } else {
@@ -129,6 +152,16 @@ const MoviesList = (props) => {
             );
           })}
         </Row>
+        <br />
+        Showing page: {currentPage}.
+        <Button
+          variant="link"
+          onClick={() => {
+            setCurrentPage(currentPage + 1);
+          }}
+        >
+          Get next {entriesPerPage} results
+        </Button>
       </Container>
     </div>
   );
